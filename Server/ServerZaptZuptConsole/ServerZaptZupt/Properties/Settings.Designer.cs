1dba847a4601008e964f6c4bad04e1c945716aad
Guilherme Ramos <gmatheus95@outlook.com>
2016-12-20 01:16:16 -0200

    Minor layout fix

--- Server/ServerZaptZuptConsole/ServerZaptZupt/Properties/Settings.Designer.cs
+++ Server/ServerZaptZuptConsole/ServerZaptZupt/Properties/Settings.Designer.cs
@@ -44,5 +44,16 @@ namespace ServerZaptZupt.Properties {
                 return ((string)(this["Database1ConnectionString1"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\bin\\Release\\D" +
+            "atabase1.mdf;Integrated Security=True;Connect Timeout=30")]
+        public string Database1ConnectionString2 {
+            get {
+                return ((string)(this["Database1ConnectionString2"]));
+            }
+        }
     }
 }
